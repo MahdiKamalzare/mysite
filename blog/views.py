44ff6092690afd2ca4572f59b201de683fa21d99
MahdiKamalzare <mahdikamalzaree@gmail.com>
2025-11-20 13:17:55 +0330

    /

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,14 @@ from blog.models import Post
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter("status = 1")
+    posts = Post.objects.filter(status = 1)
     context = {"posts" : posts}
     return render(request , 'blog/blog-home.html' , context)
-def blog_single(request):
-    return render(request , 'blog/blog-single.html')
+def blog_single(request , pid):
+    posts = Post.objects.filter(status = 1)
+    post = get_object_or_404(posts , pk = pid)
+    context = {"post" : post}
+    return render(request , 'blog/blog-single.html' , context)
 def test(request , pid):
     # post = Post.objects.get(id = pid)
     post = get_object_or_404(Post , pk = pid)
