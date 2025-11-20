60d2e0084ce9b733ba73bb5f43f5777bbc3d22a1
MahdiKamalzare <mahdikamalzaree@gmail.com>
2025-11-21 01:30:50 +0330

    added category

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post , Category
 # Register your models here.
 
 @admin.register(Post)
@@ -11,4 +11,6 @@ class PostAdmin(admin.ModelAdmin):
     # ordering = ["-created_date"]
     search_fields = ["title" , "content"]
 # admin.site.register(Post , PostAdmin)
+admin.site.register(Category)
+
 
