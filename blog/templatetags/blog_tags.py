e78f313250a6cba6e52f5d4103f1620048912731
MahdiKamalzare <mahdikamalzaree@gmail.com>
2025-11-26 22:18:22 +0330

    .

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,9 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+@register.inclusion_tag("popularposts.html")
+def popularposts():
+    posts = Post.objects.filter(status = 1).order_by("published_date")
+    return {"posts" : posts}
