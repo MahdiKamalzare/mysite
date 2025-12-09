7ea10cfbd50e27f61795877117665e496a6b0739
MahdiKamalzare <mahdikamalzaree@gmail.com>
2025-12-10 01:32:41 +0330

    .

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from blog.models import Post , Category
+from blog.models import Post , Category , Comment
 from django_summernote.admin import SummernoteModelAdmin
+
 # Register your models here.
 
 @admin.register(Post)
@@ -12,6 +13,14 @@ class PostAdmin(SummernoteModelAdmin):
     # ordering = ["-created_date"]
     search_fields = ["title" , "content"]
     summernote_fields = ('content',)
+
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-"
+    list_display = ("name" , "post" , "approved" , "created_date")
+    list_filter = ("post" , "approved",)
+    search_fields = ["name" , "post"]
+admin.site.register(Comment , CommentAdmin)
 admin.site.register(Category)
 
 
