1946d4bc371912f93b46f6bd2bbbeb00da30280c
MahdiKamalzare <mahdikamalzaree@gmail.com>
2025-12-09 13:33:07 +0330

    .

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -21,6 +22,11 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_date"]
+        ordering = ["-created_date"]
     def __str__(self):
-        return "{} - {}".format( self.title , self.id)
+        # return "{} - {}".format( self.title , self.id)
+        return self.title
+    def snippets(self):
+        return self.content[: 100] + "..."
+    def get_absolute_url(self):
+        return reverse("blog:single" , kwargs = {"pid" : self.id})
