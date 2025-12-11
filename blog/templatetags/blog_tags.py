d03d6bc0863bd5e67383e3e59a1a81b1b00777f4
MahdiKamalzare <mahdikamalzaree@gmail.com>
2025-12-11 18:52:06 +0330

    .

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post , Comment
 from blog.models import Category
 
 register = template.Library()
@@ -8,6 +8,13 @@ register = template.Library()
 def latestposts():
     posts = Post.objects.filter(status = 1).order_by("published_date")
     return {"posts" : posts}
+
+
+@register.simple_tag(name = "comments_count")
+def function(pid):
+    post = Post.objects.get(pk = pid)
+    return Comment.objects.filter(post=post.id , approved = True).count()
+    context = {"post" : post , "comments" : comments}
 
 @register.inclusion_tag("blog/blog-post-categories.html")
 def postcategories():
